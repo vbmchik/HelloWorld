58e2cd125807d1dc322341662117343f3b7cb4b2
vbmchik@gmail.com <38gjgeuftd>
2017-11-10 16:06:16 +0300

    GCD added for all cases think all is fixed

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        
+
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22};
         int[] array1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18};
-        int n = 14;
+        int n = 18;
         //сдвиг влево
         //ShiftArray(array, 4);
         System.out.println(Arrays.toString(array));
