2e21754f13a09fc3058410de7df1794825dd7920
vbmchik@gmail.com <38gjgeuftd>
2017-11-13 17:24:18 +0300

    Shift overweight array length is processed optimally

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28};
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23};//, 24, 25, 26, 27, 28};
         int[] array1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18};
-        int n = -29;
+        int n = 5;
 
         System.out.println(Arrays.toString(array));
         mover(array, n);
